6f3036e56ef242c18d002fd18fa561fcb6b76aa0
sudo-gera <gera.tatarinov.999@gmail.com>
2023-02-12 18:16:59 +0300

    *interesting commit message*

--- nasm_get_vm_address.sh
+++ nasm_get_vm_address.sh
@@ -2,6 +2,7 @@ function bad_addr(){
     echo -e "\x1b[91mcannot connect to $(cat ~/.nasm_vm_address.txt)\x1b[0m"
     echo -e "\x1b[95mgive me vm address (you can see it inside virtual machine)\x1b[0m"
     echo -e "\x1b[94mleave empty to try again with $(cat ~/.nasm_vm_address.txt)\x1b[0m"
+    echo -e "\x1b[93mhit ctrl+C to cancel\x1b[0m"
     read _addr
     if [ "$_addr" != "" ]
     then
--- ngi.sh
+++ ngi.sh
@@ -1,25 +1,26 @@
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+function install(){
+    curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/nasm_get_vm_address.sh" > ~/nasm_get_vm_address.sh
+    chmod 744 ~/nasm_get_vm_address.sh
 
-curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/nasm_get_vm_address.sh" > ~/nasm_get_vm_address.sh
-chmod 744 ~/nasm_get_vm_address.sh
+    echo "192.168.64.21" > ~/.nasm_vm_address.txt
+    ~/nasm_get_vm_address.sh
 
-echo "192.168.64.21" > ~/.nasm_vm_address.txt
-~/nasm_get_vm_address.sh
+    curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/nasm.sh" > ~/nasm.sh
+    chmod 744 ~/nasm.sh
 
-curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/nasm.sh" > ~/nasm.sh
-chmod 744 ~/nasm.sh
+    curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/nasm.sh" > ~/nasm.sh
+    chmod 744 ~/nasm.sh
 
-curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/nasm.sh" > ~/nasm.sh
-chmod 744 ~/nasm.sh
+    mkdir -p ~/.ssh/
+    chmod 700 ~/.ssh
 
-mkdir -p ~/.ssh/
-chmod 700 ~/.ssh
+    curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/id_rsa_for_nasm.b64" | openssl base64 -d > ~/.ssh/id_rsa_for_nasm
+    chmod 600 ~/.ssh/id_rsa_for_nasm
 
-curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/id_rsa_for_nasm.b64" | openssl base64 -d > ~/.ssh/id_rsa_for_nasm
-chmod 600 ~/.ssh/id_rsa_for_nasm
-
-curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/id_rsa_for_nasm.b64" > ~/.ssh/id_rsa_for_nasm.pub
-chmod 644 ~/.ssh/id_rsa_for_nasm
+    curl -s "https://raw.githubusercontent.com/sudo-gera/linux-nasm-for-macos/main/id_rsa_for_nasm.b64" > ~/.ssh/id_rsa_for_nasm.pub
+    chmod 644 ~/.ssh/id_rsa_for_nasm
 
 
-echo -e "\x1b[92mNice! Now you can use \x1b[94m~/nasm.sh\x1b[92m command! Let's run it now!\x1b[0m"
+    echo -e "\x1b[92mNice! Now you can use \x1b[94m~/nasm.sh\x1b[92m command! Let's run it now!\x1b[0m"
+}
+install
